580b21f04c6e1e852d04e24cfa40c68a2af1aa56
afan <alzafarx@gmail.com>
2023-10-03 20:42:01 +0700

    exercises Cpp

---
+++ arithmeticOperator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2, res;
+
+    num1 = 7;
+    num2 = 8;
+
+    res = num1 + num2;
+
+    cout<<res<<endl;
+
+    cout<<"enter first number : ";
+    cin>>num1;
+    cout<<"enter second number : ";
+    cin>>num2;
+    res = num1 + num2;
+    cout<<res;
+}
